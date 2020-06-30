41611fa84dc3187ba20fbcb679075387d9ae4f8e
Leonardo Prati <leonardo@sflabs.com.br>
2020-06-30 02:43:46 -0300

    Adiciona rotas para ler e alterar eventos

---
+++ routes/eventos.routes.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const calendar = require('../utils/calendar');
+const queries = require('../db/queries');
+
+const router = express.Router();
+
+router.get('/eventos/:instituto', async (req, res) => {
+    try {
+        let listaDeEventos = await calendar.listaEventosDisponiveis(req.params.instituto);
+        if (listaDeEventos) {
+            res.status(200).send(listaDeEventos);
+        }
+        else {
+            res.status(404).send("Nenhum evento encontrado");
+        }
+    }
+    catch (err) {
+        console.log(err.stack);
+        res.status(500).send("Error");
+    }
+});
+
+router.put('/eventos/:instituto/:idEvento', async (req, res) => {
+    try {
+        let evento = req.body.evento;
+        let emailUsuario = req.body.userEmail;
+        let resultado = calendar.atualizaEvento(req.params.instituto, evento, emailUsuario);
+        if (resultado) {
+            res.status(200).send("Sucesso");
+        }
+        else {
+            res.status(500).send("Erro");
+        }
+    }
+    catch (err) {
+        console.log(err.stack);
+        res.status(500).send("Error");
+    }
+});
+
+module.exports = router;
