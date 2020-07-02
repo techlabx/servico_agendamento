26fb7645b4880e10e1432057281bcd04b34a2e2f
Leonardo Prati <leonardo@sflabs.com.br>
2020-07-01 21:36:06 -0300

    Adiciona CORS no servico

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 'use strict';
 
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
+
 const eventos = require('./routes/eventos.routes');
 const token = require('./routes/token.routes');
 
+var corsOptions = {
+    origin: function (origin, callback) {
+        callback(null, true)
+    }
+}
+
 const app = express();
 app.use(bodyParser.json());
+app.use(cors(corsOptions));
 
 app.use('/', eventos);
 app.use('/', token)
